ade06552d1e675c7570dd653e50d5e125dfdd710
Jose Diaz <jose.diaz@arkonline.org>
2015-05-18 11:24:17 +0100

    Changes rendered files

--- public/js/widgets.js
+++ public/js/widgets.js
@@ -1076,129 +1076,6 @@
 }).call(this);
 
 (function() {
-  var bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
-    extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-    hasProp = {}.hasOwnProperty;
-
-  window.FieldContent = (function(superClass) {
-    extend(FieldContent, superClass);
-
-    function FieldContent() {
-      this.updateMapping = bind(this.updateMapping, this);
-      this.changeMapping = bind(this.changeMapping, this);
-      return FieldContent.__super__.constructor.apply(this, arguments);
-    }
-
-    FieldContent.className = "FieldContent";
-
-    FieldContent.displayName = "Dynamic Text";
-
-    FieldContent.description = "Pull a text field from a student record and style it for display.";
-
-    FieldContent.icon = "nameplate";
-
-    FieldContent.prototype.defaultWidth = function() {
-      return 200;
-    };
-
-    FieldContent.prototype.defaultHeight = function() {
-      return 50;
-    };
-
-    FieldContent.STYLE_DEFAULTS = {
-      color: '#000000',
-      font: 'Helvetica',
-      size: 'Medium'
-    };
-
-    FieldContent.prototype.initWithConfig = function(config) {
-      this._field = this.get(config.field, Object.keys(this.metrics())[0]);
-      this.style = $.extend({}, FieldContent.STYLE_DEFAULTS, this.get(config.style, {}));
-      return this.mappings = this.get(config.mappings, {});
-    };
-
-    FieldContent.prototype.render_layout = function(data) {
-      return $("<div class=\"field-widget\" style=\"" + (this.textStyles()) + "\">" + (this.fieldFrom(data)) + "</div>");
-    };
-
-    FieldContent.prototype.renderAppearanceOptions = function() {
-      return this.option('font', 'font', "Font") + this.option('size', 'size', "Text Size") + this.option('color', 'color', "Text Color");
-    };
-
-    FieldContent.prototype.renderConfigOptions = function() {
-      return [
-        this.option('select', 'field', "Field", {
-          options: this.metrics(),
-          hint: "This is the CCR! field you would like to be merged, the data shown is only a sample of the final output."
-        }), this.mappingSettings()
-      ];
-    };
-
-    FieldContent.prototype.mappingSettings = function() {
-      var node, self;
-      node = $("<div class=\"mapping-option\"><a href=\"#\" class=\"mapping\">" + ($.isEmptyObject(this.mappings) ? 'Add word mappings...' : 'Edit word mappings...') + "</a></div>");
-      self = this;
-      node.on("click", ".mapping", (function(_this) {
-        return function() {
-          return new MappingModal(_this.mappings, _this.changeMapping);
-        };
-      })(this));
-      return node;
-    };
-
-    FieldContent.prototype.changeMapping = function(newMappings) {
-      var oldMappings;
-      oldMappings = this.mappings;
-      this.updateMapping(newMappings);
-      return Designer.history.push(this, 'updateMapping', oldMappings, newMappings);
-    };
-
-    FieldContent.prototype.updateMapping = function(mappings) {
-      this.mappings = mappings;
-      return this.redraw();
-    };
-
-    FieldContent.prototype.textStyles = function() {
-      return this.styleString({
-        'color': this.style.color,
-        'font-family': utils.fontMap[this.style.font],
-        'font-size': utils.sizeMap[this.style.size]
-      });
-    };
-
-    FieldContent.prototype.fieldFrom = function(data) {
-      var i, key, len, ref;
-      ref = this._field.split('.');
-      for (i = 0, len = ref.length; i < len; i++) {
-        key = ref[i];
-        data = data != null ? data[key] : void 0;
-      }
-      return this.mappings[data] || data || this.placeholderWithLabel(key);
-    };
-
-    FieldContent.prototype.font = FieldContent.property('style', 'font');
-
-    FieldContent.prototype.size = FieldContent.property('style', 'size');
-
-    FieldContent.prototype.color = FieldContent.property('style', 'color');
-
-    FieldContent.prototype.field = FieldContent.property('_field');
-
-    FieldContent.prototype.serialize = function() {
-      return {
-        field: this._field,
-        style: this.style,
-        mappings: this.mappings
-      };
-    };
-
-    return FieldContent;
-
-  })(WidgetContent);
-
-}).call(this);
-
-(function() {
   var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
     hasProp = {}.hasOwnProperty;
 
@@ -1336,113 +1213,6 @@
     };
 
     return ImageContent;
-
-  })(WidgetContent);
-
-}).call(this);
-
-(function() {
-  var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-    hasProp = {}.hasOwnProperty;
-
-  window.ImageGalleryContent = (function(superClass) {
-    extend(ImageGalleryContent, superClass);
-
-    function ImageGalleryContent() {
-      return ImageGalleryContent.__super__.constructor.apply(this, arguments);
-    }
-
-    ImageGalleryContent.className = "ImageGalleryContent";
-
-    ImageGalleryContent.displayName = "Dynamic Images";
-
-    ImageGalleryContent.description = "A dynamic photograph, logo or graphic";
-
-    ImageGalleryContent.icon = "picture";
-
-    ImageGalleryContent.prototype.editable = function() {
-      return this.widget.currentMode === 'layout';
-    };
-
-    ImageGalleryContent.prototype.initWithConfig = function(config) {
-      this._field = this.get(config.field, Object.keys(this.images())[0]);
-      return this._stretchImage = this.get(config.stretchImage, true);
-    };
-
-    ImageGalleryContent.prototype.bindEvents = function(el) {
-      return this.widget.bind('widget:resize', (function(_this) {
-        return function() {
-          return _this.imageCSS();
-        };
-      })(this));
-    };
-
-    ImageGalleryContent.prototype.imageCSS = function() {
-      $('.image-gallery-widget img').removeClass(this.cssImageClass);
-      this.calcImageCSS();
-      return $('.image-gallery-widget img').addClass(this.cssImageClass);
-    };
-
-    ImageGalleryContent.prototype.calcImageCSS = function() {
-      if (this.width >= this.height && (this.stretchImage() || this.width > this.widget.width())) {
-        return this.cssImageClass = 'wider';
-      } else if (this.height > this.width && (this.stretchImage() || this.height > this.widget.height())) {
-        return this.cssImageClass = 'taller';
-      } else {
-        return this.cssImageClass = '';
-      }
-    };
-
-    ImageGalleryContent.prototype.render_layout = function(data) {
-      var img;
-      img = new Image();
-      img.src = "data:image/gif;base64," + (this.fieldFrom(data));
-      img.width / img.height;
-      this.width = img.width;
-      this.height = img.height;
-      this.calcImageCSS();
-      if (this.editable()) {
-        setTimeout(((function(_this) {
-          return function() {
-            return _this.setAspectRatio(1);
-          };
-        })(this)), 0);
-      }
-      return $("<div class=\"image-gallery-widget\">\n  <img class=\"content " + this.cssImageClass + "\" src=\"" + img.src + "\">\n</div>");
-    };
-
-    ImageGalleryContent.prototype.renderConfigOptions = function() {
-      return [
-        this.option('checkbox', 'stretchImage', "Stretch Image"), this.option('select', 'field', "Field", {
-          options: this.images(),
-          hint: "This is the gallery image you would like to be merged, the image shown is only a placeholder of the final output."
-        })
-      ];
-    };
-
-    ImageGalleryContent.prototype.stretchImage = ImageGalleryContent.property('_stretchImage');
-
-    ImageGalleryContent.prototype.fieldFrom = function(data) {
-      var i, key, len, ref, ref1, results;
-      ref = this._field.split('.');
-      results = [];
-      for (i = 0, len = ref.length; i < len; i++) {
-        key = ref[i];
-        results.push(data = (ref1 = data['images']) != null ? ref1[key] : void 0);
-      }
-      return results;
-    };
-
-    ImageGalleryContent.prototype.field = ImageGalleryContent.property('_field');
-
-    ImageGalleryContent.prototype.serialize = function() {
-      return {
-        field: this._field,
-        stretchImage: this.stretchImage()
-      };
-    };
-
-    return ImageGalleryContent;
 
   })(WidgetContent);
 
@@ -1590,6 +1360,129 @@
     extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
     hasProp = {}.hasOwnProperty;
 
+  window.FieldContent = (function(superClass) {
+    extend(FieldContent, superClass);
+
+    function FieldContent() {
+      this.updateMapping = bind(this.updateMapping, this);
+      this.changeMapping = bind(this.changeMapping, this);
+      return FieldContent.__super__.constructor.apply(this, arguments);
+    }
+
+    FieldContent.className = "FieldContent";
+
+    FieldContent.displayName = "Dynamic Text";
+
+    FieldContent.description = "Pull a text field from a student record and style it for display.";
+
+    FieldContent.icon = "nameplate";
+
+    FieldContent.prototype.defaultWidth = function() {
+      return 200;
+    };
+
+    FieldContent.prototype.defaultHeight = function() {
+      return 50;
+    };
+
+    FieldContent.STYLE_DEFAULTS = {
+      color: '#000000',
+      font: 'Helvetica',
+      size: 'Medium'
+    };
+
+    FieldContent.prototype.initWithConfig = function(config) {
+      this._field = this.get(config.field, Object.keys(this.metrics())[0]);
+      this.style = $.extend({}, FieldContent.STYLE_DEFAULTS, this.get(config.style, {}));
+      return this.mappings = this.get(config.mappings, {});
+    };
+
+    FieldContent.prototype.render_layout = function(data) {
+      return $("<div class=\"field-widget\" style=\"" + (this.textStyles()) + "\">" + (this.fieldFrom(data)) + "</div>");
+    };
+
+    FieldContent.prototype.renderAppearanceOptions = function() {
+      return this.option('font', 'font', "Font") + this.option('size', 'size', "Text Size") + this.option('color', 'color', "Text Color");
+    };
+
+    FieldContent.prototype.renderConfigOptions = function() {
+      return [
+        this.option('select', 'field', "Field", {
+          options: this.metrics(),
+          hint: "This is the CCR! field you would like to be merged, the data shown is only a sample of the final output."
+        }), this.mappingSettings()
+      ];
+    };
+
+    FieldContent.prototype.mappingSettings = function() {
+      var node, self;
+      node = $("<div class=\"mapping-option\"><a href=\"#\" class=\"mapping\">" + ($.isEmptyObject(this.mappings) ? 'Add word mappings...' : 'Edit word mappings...') + "</a></div>");
+      self = this;
+      node.on("click", ".mapping", (function(_this) {
+        return function() {
+          return new MappingModal(_this.mappings, _this.changeMapping);
+        };
+      })(this));
+      return node;
+    };
+
+    FieldContent.prototype.changeMapping = function(newMappings) {
+      var oldMappings;
+      oldMappings = this.mappings;
+      this.updateMapping(newMappings);
+      return Designer.history.push(this, 'updateMapping', oldMappings, newMappings);
+    };
+
+    FieldContent.prototype.updateMapping = function(mappings) {
+      this.mappings = mappings;
+      return this.redraw();
+    };
+
+    FieldContent.prototype.textStyles = function() {
+      return this.styleString({
+        'color': this.style.color,
+        'font-family': utils.fontMap[this.style.font],
+        'font-size': utils.sizeMap[this.style.size]
+      });
+    };
+
+    FieldContent.prototype.fieldFrom = function(data) {
+      var i, key, len, ref;
+      ref = this._field.split('.');
+      for (i = 0, len = ref.length; i < len; i++) {
+        key = ref[i];
+        data = data != null ? data[key] : void 0;
+      }
+      return this.mappings[data] || data || this.placeholderWithLabel(key);
+    };
+
+    FieldContent.prototype.font = FieldContent.property('style', 'font');
+
+    FieldContent.prototype.size = FieldContent.property('style', 'size');
+
+    FieldContent.prototype.color = FieldContent.property('style', 'color');
+
+    FieldContent.prototype.field = FieldContent.property('_field');
+
+    FieldContent.prototype.serialize = function() {
+      return {
+        field: this._field,
+        style: this.style,
+        mappings: this.mappings
+      };
+    };
+
+    return FieldContent;
+
+  })(WidgetContent);
+
+}).call(this);
+
+(function() {
+  var bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    hasProp = {}.hasOwnProperty;
+
   window.SubjectFieldContent = (function(superClass) {
     extend(SubjectFieldContent, superClass);
 
@@ -1705,6 +1598,130 @@
   var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
     hasProp = {}.hasOwnProperty;
 
+  window.ImageGalleryContent = (function(superClass) {
+    extend(ImageGalleryContent, superClass);
+
+    function ImageGalleryContent() {
+      return ImageGalleryContent.__super__.constructor.apply(this, arguments);
+    }
+
+    ImageGalleryContent.className = "ImageGalleryContent";
+
+    ImageGalleryContent.displayName = "Dynamic Images";
+
+    ImageGalleryContent.description = "A dynamic photograph, logo or graphic";
+
+    ImageGalleryContent.icon = "picture";
+
+    ImageGalleryContent.prototype.editable = function() {
+      return this.widget.currentMode === 'layout';
+    };
+
+    ImageGalleryContent.prototype.initWithConfig = function(config) {
+      this._field = this.get(config.field, Object.keys(this.images())[0]);
+      return this._stretchImage = this.get(config.stretchImage, true);
+    };
+
+    ImageGalleryContent.prototype.bindEvents = function(el) {
+      return this.widget.bind('widget:resize', (function(_this) {
+        return function() {
+          return _this.imageCSS();
+        };
+      })(this));
+    };
+
+    ImageGalleryContent.prototype.imageCSS = function() {
+      $('.image-gallery-widget img').removeClass(this.cssImageClass);
+      this.calcImageCSS();
+      return $('.image-gallery-widget img').addClass(this.cssImageClass);
+    };
+
+    ImageGalleryContent.prototype.calcImageCSS = function() {
+      if (this.width >= this.height && (this.stretchImage() || this.width > this.widget.width())) {
+        return this.cssImageClass = 'wider';
+      } else if (this.height > this.width && (this.stretchImage() || this.height > this.widget.height())) {
+        return this.cssImageClass = 'taller';
+      } else {
+        return this.cssImageClass = '';
+      }
+    };
+
+    ImageGalleryContent.prototype.render_layout = function(data) {
+      var img;
+      img = new Image();
+      img.src = "data:image/gif;base64," + (this.fieldFrom(data));
+      img.width / img.height;
+      this.width = img.width;
+      this.height = img.height;
+      this.calcImageCSS();
+      if (this.editable()) {
+        setTimeout(((function(_this) {
+          return function() {
+            return _this.setAspectRatio(1);
+          };
+        })(this)), 0);
+      }
+      return $("<div class=\"image-gallery-widget\">\n  <img class=\"content " + this.cssImageClass + "\" src=\"" + img.src + "\">\n</div>");
+    };
+
+    ImageGalleryContent.prototype.renderConfigOptions = function() {
+      return [
+        this.option('checkbox', 'stretchImage', "Stretch Image"), this.option('select', 'field', "Field", {
+          options: this.images(),
+          hint: "This is the gallery image you would like to be merged, the image shown is only a placeholder of the final output."
+        })
+      ];
+    };
+
+    ImageGalleryContent.prototype.stretchImage = ImageGalleryContent.property('_stretchImage');
+
+    ImageGalleryContent.prototype.fieldFrom = function(data) {
+      var i, key, len, ref, ref1, results;
+      ref = this._field.split('.');
+      results = [];
+      for (i = 0, len = ref.length; i < len; i++) {
+        key = ref[i];
+        results.push(data = (ref1 = data['images']) != null ? ref1[key] : void 0);
+      }
+      return results;
+    };
+
+    ImageGalleryContent.prototype.field = ImageGalleryContent.property('_field');
+
+    ImageGalleryContent.prototype.serialize = function() {
+      return {
+        field: this._field,
+        stretchImage: this.stretchImage()
+      };
+    };
+
+    return ImageGalleryContent;
+
+  })(WidgetContent);
+
+}).call(this);
+
+(function() {
+  var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    hasProp = {}.hasOwnProperty;
+
+  window._EXAMPLEContent = (function(superClass) {
+    extend(_EXAMPLEContent, superClass);
+
+    function _EXAMPLEContent() {
+      return _EXAMPLEContent.__super__.constructor.apply(this, arguments);
+    }
+
+    return _EXAMPLEContent;
+
+  })(WidgetContent);
+
+}).call(this);
+
+(function() {
+  var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    hasProp = {}.hasOwnProperty;
+
   window.TextContent = (function(superClass) {
     extend(TextContent, superClass);
 
@@ -1789,23 +1806,6 @@
     };
 
     return TextContent;
-
-  })(WidgetContent);
-
-}).call(this);
-
-(function() {
-  var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-    hasProp = {}.hasOwnProperty;
-
-  window._EXAMPLEContent = (function(superClass) {
-    extend(_EXAMPLEContent, superClass);
-
-    function _EXAMPLEContent() {
-      return _EXAMPLEContent.__super__.constructor.apply(this, arguments);
-    }
-
-    return _EXAMPLEContent;
 
   })(WidgetContent);
 
