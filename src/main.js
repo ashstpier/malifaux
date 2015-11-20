f37de259705e77bf4c3f35d1ce55fb872406f52b
Andy Kent <andy.kent@me.com>
2015-11-20 07:16:58 +0000

    added parent reports css and a header element

--- src/main.js
+++ src/main.js
@@ -16,9 +16,9 @@ if(__DEV__) {
     // Enables your middleware:
     // applyMiddleware(m1, m2, m3), // any Redux middleware, e.g. redux-thunk
     // Provides support for DevTools:
-    devTools(),
+    devTools()
     // Lets you write ?debug_session=<name> in address bar to persist debug sessions
-    persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+    // persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
   )(createStore)
 
   const store = finalCreateStore(todoApp)
--- src/reducers.js
+++ src/reducers.js
@@ -1,42 +1,19 @@
 import { combineReducers } from 'redux'
-import { ADD_TODO, COMPLETE_TODO, SET_VISIBILITY_FILTER, VisibilityFilters } from './actions'
-const { SHOW_ALL } = VisibilityFilters
+import { SET_TITLE } from './actions'
 
-function visibilityFilter(state = SHOW_ALL, action) {
+const UNTITLED = "Untitled"
+
+export function title(state = UNTITLED, action) {
   switch (action.type) {
-    case SET_VISIBILITY_FILTER:
-      return action.filter
+    case SET_TITLE:
+      return action.text
     default:
       return state
   }
 }
 
-function todos(state = [], action) {
-  switch (action.type) {
-    case ADD_TODO:
-      return [
-        ...state,
-        {
-          text: action.text,
-          completed: false
-        }
-      ]
-    case COMPLETE_TODO:
-      return [
-        ...state.slice(0, action.index),
-        Object.assign({}, state[action.index], {
-          completed: true
-        }),
-        ...state.slice(action.index + 1)
-      ]
-    default:
-      return state
-  }
-}
-
-const todoApp = combineReducers({
-  visibilityFilter,
-  todos
+const reportsApp = combineReducers({
+  title
 })
 
-export default todoApp
+export default reportsApp
